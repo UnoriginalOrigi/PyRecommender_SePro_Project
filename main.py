07fc774fed19d6baa1c886c2f404a2becee953b0
UnoriginalOrigi <105780035+UnoriginalOrigi@users.noreply.github.com>
2024-03-19 18:39:13 +0200

    added login and basic search functionality

--- main.py
+++ main.py
@@ -26,31 +26,68 @@ def action_input():
 
     return action
 
+def credential_input():
+    try:
+        print("Input your client id token:")
+        client_id_input = input()
+        if len(client_id_input) > MAXIMUM_INPUT_LENGTH:
+            raise OverflowError
+        print("Input your client secret token:")
+        client_secret_input = input()
+        if len(client_secret_input) > MAXIMUM_INPUT_LENGTH:
+            raise OverflowError
+        with open("client_info.txt","w") as f:
+            f.write(client_id_input)
+            f.write("\n")
+            f.write(client_secret_input)
+        client_id = client_id_input
+        client_secret = client_secret_input
+    except OverflowError:
+        print("Error 2: Input higher than maximum possible length")
+        client_id = "-1"
+        client_secret = "-1"
+    
+    return client_id, client_secret
+
+def credential_loader():
+    with open("client_info.txt","r") as f:
+        client_id = f.readline()
+        client_id = client_id.strip()
+        client_secret = f.readline()
+        client_secret = client_secret.strip()
+    return client_id, client_secret
+
 def main():
     print("Welcome to the Spotify Recommender Prototype!")
-
     while(True):
         action = action_input()
         if action == 0:
             print("Program end!")
             break
         elif action == 1:
-            print("Input your client id token")
+            if os.path.exists("client_info.txt"):
+                while True:
+                    print("Saved credentials found, use saved credentials? y/n")
+                    input_action = input().lower()
+                    if input_action == 'y':
+                        cls()
+                        print("Using saved credentials")
+                        client_id, client_secret = credential_loader()
+                        break
+                    elif input_action == 'n':
+                        cls()
+                        client_id, client_secret = credential_input()
+                        break
+                    else:
+                        print("Invalid input")
             try:
-                client_id_input = input()
-                if len(client_id_input) > MAXIMUM_INPUT_LENGTH:
-                    raise OverflowError
-                print("Input your client secret token")
-                client_secret_input = input()
-                if len(client_secret_input) > MAXIMUM_INPUT_LENGTH:
-                    raise OverflowError
-                with open("client_info.txt","w") as f:
-                    f.write(client_id_input)
-                    f.write("\n")
-                    f.write(client_secret_input)
-            except OverflowError:
-                print("Error 2: Input higher than maximum possible length")
-
+                sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,client_secret=client_secret))
+                search_query = input("Login successful, enter search query: ")
+                results = sp.search(q=search_query, limit=20)
+                for idx, track in enumerate(results['tracks']['items']):
+                    print(idx+1, track['name'])
+            except spotipy.oauth2.SpotifyOauthError:
+                print("Invalid Credentials Given, failed to connect")
         elif action == -1:
             print("Error encountered")
         else:
