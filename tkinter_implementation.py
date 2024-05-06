5a45c9ee3a33b3cc6683d0f721b08e9615ec573e
UnoriginalOrigi <105780035+UnoriginalOrigi@users.noreply.github.com>
2024-05-06 12:00:35 +0300

    Error Handling, Tkinter Song Search

--- tkinter_implementation.py
+++ tkinter_implementation.py
@@ -38,6 +38,8 @@ class __Login_Window:
 
         self.master.update()
 
+        self.label4 = ttk.Label(self.frame, text="Attempt to Login",foreground='red')
+
         self.label1 = ttk.Label(self.frame, text="Client ID")
         self.entry_text1 = StringVar()
         self.entry1 = ttk.Entry(self.frame,width=35,textvariable=self.entry_text1)
@@ -50,6 +52,7 @@ class __Login_Window:
         self.entry2.place(x=self.frame.winfo_width()/2-102.5, y=self.frame.winfo_height()/2+30)
         self.label1.place(x=self.frame.winfo_width()/2-102.5, y=self.frame.winfo_height()/2-52)
         self.label2.place(x=self.frame.winfo_width()/2-102.5, y=self.frame.winfo_height()/2+8)
+        self.label4.place(x=self.frame.winfo_width()/2-102.5, y=self.frame.winfo_height()/2+60)
         self.master.update()
 
 
@@ -58,12 +61,16 @@ class __Login_Window:
             txt_entry.set(txt_entry.get()[:EXPECTED_INPUT_LENGTH])
 
     def __login_window(self):
-        try:
-            self.sp = login_attempt(self.entry1.get(),self.entry2.get())
-            self.__logged_in = True
-            self.master.destroy()
-        except spotipy.oauth2.SpotifyOauthError:
-                print("Invalid Credentials Given, failed to connect")
+        if len(self.entry1.get()) < EXPECTED_INPUT_LENGTH or len(self.entry2.get()) < EXPECTED_INPUT_LENGTH:
+            self.label4.config(text="Invalid Credentials. Input too short.")
+        else:
+            try:
+                self.sp = login_attempt(self.entry1.get(),self.entry2.get())
+                self.__logged_in = True
+                self.master.destroy()
+            except spotipy.oauth2.SpotifyOauthError:
+                self.label4.config(text="Invalid Credentials. Check Credentials")
+                self.label4.place()
 
     def get_login_status(self):
          return self.__logged_in, self.sp
@@ -76,7 +83,8 @@ class __Login_Window:
             self.__logged_in = True
             self.master.destroy()
         except spotipy.oauth2.SpotifyOauthError:
-                print("Invalid Credentials Given, failed to connect")
+            self.label4.config(text="Invalid Credentials. File Corrupted or Key Invalid")
+            self.label4.place()
 
 class __Program_Window:
     def __init__(self,root,sp):
@@ -96,20 +104,31 @@ class __Program_Window:
 
         self.label1 = ttk.Label(self.frame, text="Search Input ")
         self.label2 = ttk.Label(self.frame, text="Invalid Input ",state=DISABLED)
+        #self.label3 = ttk.Label(self.frame, text="Currently playing: None")
+        self.label1.place(x=self.frame.winfo_width()-280, y=self.frame.winfo_height()-48)
+        #self.label3.place(x=5, y=self.frame.winfo_height()-48)
+
+        self.dropbox_options = ["Artist","Song"]
+        self.dropbox_text = StringVar()
+        self.dropbox_text.set("Artist")
+        self.dropbox1 = OptionMenu(self.master,self.dropbox_text,*self.dropbox_options)
+        self.dropbox1.place(x=self.frame.winfo_width()-360, y=self.frame.winfo_height()-44)
+
         self.entry_text1 = StringVar()
         self.entry1 = ttk.Entry(self.frame,width=30,textvariable=self.entry_text1)
         self.entry_text1.trace_add("write", lambda *args: self.__character_limit(self.entry_text1))
-        self.label1.place(x=self.frame.winfo_width()-280, y=self.frame.winfo_height()-48)
         self.entry1.place(x=self.frame.winfo_width()-280, y=self.frame.winfo_height()-30)
 
-        self.button1 = ttk.Button(self.frame, text="Find", command=self.__related_artists)
+        self.button1 = ttk.Button(self.frame, text="Find", command=self.__related_search)
         self.button2 = ttk.Button(self.frame, text="Quit", command=self.master.destroy)
         self.button3 = ttk.Button(self.frame, text=">", width=5, command=self.__get_cursor_songs, state=DISABLED)
         self.button4 = ttk.Button(self.frame, text="<", width=5, command=self.__get_related_artists, state=DISABLED)
+        #self.button5 = ttk.Button(self.frame, text="Play", command=self.__play_selected_song, state=DISABLED)
         self.button1.place(x=self.frame.winfo_width()-80, y=self.frame.winfo_height()-30)
         self.button2.place(x=5, y=self.frame.winfo_height()-30)
         self.button3.place(x=self.frame.winfo_width()/2-105, y=self.frame.winfo_height()/2-130)
         self.button4.place(x=self.frame.winfo_width()/2-105, y=self.frame.winfo_height()/2-70)
+        #self.button5.place(x=self.frame.winfo_width()/2-30, y=self.frame.winfo_height()-30)
 
     def __get_related_artists(self):
         try:
@@ -130,7 +149,17 @@ class __Program_Window:
     def __get_cursor_songs(self):
         try:
             self.button4['state'] = NORMAL
-            results = self.sp.search(q=self.Lb1.get(self.Lb1.curselection()[0]), limit=20)
+            #self.button5['state'] = NORMAL
+            search_query = re.search("(?<=~ ).*?(?= &)",self.Lb1.get(self.Lb1.curselection()[0]))
+            try:
+                search_query = search_query.group(0)
+            except AttributeError:
+                search_query = re.search("(~ .+)",self.Lb1.get(self.Lb1.curselection()[0]))
+            try:
+                search_query = search_query.group(0)[2:]
+            except AttributeError:
+                search_query = self.Lb1.get(self.Lb1.curselection()[0])
+            results = self.sp.search(q=search_query, limit=20)
             self.Lb2.delete(0,END)
             for idx, track in enumerate(results['tracks']['items']):
                 temp = "{} ~ {}".format(track['name'], track['artists'][0]['name'])
@@ -145,15 +174,20 @@ class __Program_Window:
         if len(txt_entry.get()) > INPUT_SIZE:
             txt_entry.set(txt_entry.get()[:INPUT_SIZE])
 
-
-    def __related_artists(self):
+    def __related_search(self):
+        print(self.dropbox_text.get())
         if len(self.entry1.get()) > 0 and len(self.entry1.get()) < INPUT_SIZE:
             self.label2.place_forget()
-            related_artists = related_artists_search(self.sp,self.entry1.get())
             self.Lb1.delete(0,END)
-            self.button3['state'] = NORMAL
-            for idx, artist in enumerate(related_artists['artists']):
-                self.Lb1.insert(idx+1,artist['name'])
+            if(self.dropbox_text.get() == "Artist"):
+                related_artists = related_artists_search(self.sp,self.entry1.get())
+                self.button3['state'] = NORMAL
+                for idx, artist in enumerate(related_artists['artists']):
+                    self.Lb1.insert(idx+1,artist['name'])
+            else:
+                results = self.sp.search(q=self.entry1.get(), limit=20)
+                for idx, track in enumerate(results['tracks']['items']):
+                    self.Lb1.insert(idx+1,"{} ~ {}".format(track['name'], track['artists'][0]['name']))
         else:
             self.label2.place(x=self.frame.winfo_width()-200, y=self.frame.winfo_height()-48)
 
